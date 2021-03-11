0926ee06a9396a425921a34f259e9696b43f7cd7
demonassit <jminorg@ipn.mx>
2021-03-11 09:48:31 -0600

    js amoritzacion aleman

---
+++ js/aleman.js
@@ -0,0 +1,53 @@
+const monto = document.getElementById('monto');
+const tiempo = document.getElementById('tiempo');
+const interes = document.getElementById('interes');
+const btnCalcular = document.getElementById('btnCalcular');
+const alerta = document.getElementById('alert-error');
+const llenarTabla = document.querySelector('#lista-tabla tbody');
+
+//vamos a crear una callback una funcion que manda a llamar a otra funcion
+
+btnCalcular.addEventListener('click', ()=> {
+    if(monto.value === '' || tiempo.value === '' || interes.value === ''){
+        alerta.hidden = false;
+        setTimeout(() => {
+            alerta.hidden = true;
+        }, 2000);
+    } else {
+        calcularCronograma(monto.value, tiempo.value, interes.value);
+    }
+});
+
+
+function calcularCronograma(monto, tiempo, interes) {
+
+    while(llenarTabla.firstChild){
+        llenarTabla.removeChild(llenarTabla.firstChild);
+    }
+
+    let mesActual = daysjs().add(1, 'month');
+    let amortizacionConstante, pagoInteres, cuota;
+
+    amortizacionConstante = monto / tiempo;
+
+    for(let i = 0; i <= tiempo; i++){
+        pagoInteres = monto * (interes/100);
+        cuota = amortizacionConstante + pagoInteres;
+        monto = monto - amortizacionConstante;
+
+        let fecha = mesActual.format('DD-MM-YYYY');
+        mesActual = mesActual.add(1, 'month');
+
+        //vamos a crear la tabla
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${fecha}</td>
+            <td>${amortizacionConstante.toFixed(2)}</td>
+            <td>${pagoInteres.toFixed(2)}</td>
+            <td>${cuota.toFixed(2)}</td>
+            <td>${monto.toFixed(2)}</td>
+        `;
+        llenarTabla.appendChild(row);
+    }
+    
+}
